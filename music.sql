4ad5def1bf74ba4502dbe3b2d07a009c26d90691
danny12w2 <970326MDHU00@Tameside.ac.uk>
2018-05-17 21:25:35 +0100

    dfdf

--- music.sql
+++ music.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2018 at 04:05 PM
--- Server version: 10.1.26-MariaDB
--- PHP Version: 7.1.9
+-- Generation Time: May 17, 2018 at 10:25 PM
+-- Server version: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,23 +34,22 @@ CREATE TABLE `album` (
   `SongID` int(11) NOT NULL,
   `ArtistID` int(11) NOT NULL,
   `Art` text NOT NULL,
-  `Rating` int(11) NOT NULL,
-  `songfile` text NOT NULL
+  `Rating` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `album`
 --
 
-INSERT INTO `album` (`TrackName`, `AlbumName`, `SongID`, `ArtistID`, `Art`, `Rating`, `songfile`) VALUES
-('Starman', 'Hunky Dory', 1, 1, 'hunkydory.jpg', 10, 'lifeonmars.mp3'),
-('Welcome to the Jungle', 'Appetite For Destruction', 2, 2, 'appetitefordesturction.jpg\r\n', 10, 'welcometothejungle.mp3'),
-('Rock and Roll', 'Mothership', 3, 7, 'mothership.jpg', 10, 'rockandroll.mp3'),
-('Down Payment Blues', 'Powerage', 4, 3, 'powerage.jpg', 10, 'downpaymentblues.mp3'),
-('Poison', 'Trash', 5, 4, 'trash.jpg', 10, 'poison.jpg'),
-('Love Gun', 'Love Gun', 6, 5, 'lovegun.jpg', 10, 'lovegun.mp3'),
-('Prowler', 'Iron Maiden', 7, 6, 'ironmaiden.jpg', 10, 'prowler.mp3'),
-('I Got The Blues', 'Remember My Name', 8, 8, 'remembermyname.jpg', 10, 'igottheblues.mp3');
+INSERT INTO `album` (`TrackName`, `AlbumName`, `SongID`, `ArtistID`, `Art`, `Rating`) VALUES
+('Starman', 'Hunky Dory', 1, 1, 'hunkydory.jpg', 10),
+('Welcome to the Jungle', 'Appetite For Destruction', 2, 2, 'appetitefordesturction.jpg\r\n', 10),
+('Rock and Roll', 'Mothership', 3, 7, 'mothership.jpg', 10),
+('Down Payment Blues', 'Powerage', 4, 3, 'powerage.jpg', 10),
+('Poison', 'Trash', 5, 4, 'trash.jpg', 10),
+('Love Gun', 'Love Gun', 6, 5, 'lovegun.jpg', 10),
+('Prowler', 'Iron Maiden', 7, 6, 'ironmaiden.jpg', 10),
+('I Got The Blues', 'Remember My Name', 8, 8, 'remembermyname.jpg', 10);
 
 -- --------------------------------------------------------
 
