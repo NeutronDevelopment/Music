a0c2d6ad13bbdea4f83c857c1d9cb31895f5ae5e
danny12w2 <970326MDHU00@Tameside.ac.uk>
2018-05-22 22:36:20 +0100

    ffdf

--- viewartist.php
+++ viewartist.php
@@ -2,7 +2,7 @@
 <html>
 <?php include('connect.php'); ?>
 
-        <?php $query = ("SELECT * FROM songs WHERE ArtistID="artistview);
+        <?php $query = ("SELECT * FROM songs WHERE ArtistID='2'");
         $result = mysqli_query($db, $query);
         ?> 
 
